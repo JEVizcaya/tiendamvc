a322f6c0eb0ad4f44571b2c661924cec8599cf5f
JEVizcaya <jevizcaya@hotmail.com>
2025-02-24 19:45:14 +0100

    mensaje error arreglado

--- app/views/login/login.php
+++ app/views/login/login.php
@@ -48,7 +48,7 @@
               style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
             <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="<?=base_url()?>login/register"
                 class="link-danger">Register</a></p>
-                <p><?php if(isset($data)){echo $data;}?></p>
+                <p><?php if(isset($data[0])){echo $data[0];}?></p>
           </div>
 
         </form>

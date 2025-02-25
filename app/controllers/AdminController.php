b849dba6f589e8bcc907afcdba801728918ab413
JEVizcaya <jevizcaya@hotmail.com>
2025-02-25 16:57:37 +0100

    comprobando enlace customer address

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -1,16 +1,15 @@
 <?php
 namespace Formacom\controllers;
 use Formacom\Core\Controller;
+use Formacom\Models\Customer;
 use Formacom\Models\Address;
 class AdminController extends Controller{
     public function index(...$params){
-        $address=new Address();
+        
+        $customer=Customer::find(1);
+        var_dump($customer->addresses()->first()->street);
+        exit();
        
-        $address->street="Torrecedeira nº 11";
-        $address->zip_code="36206";
-        $address->city="Vigo";
-        $address->country="España";
-        $address->save();
         $data = ['mensaje' => '¡Bienvenido a la página de inicio!'];
         $this->view('home', $data);
     }
--- app/models/Customer.php
+++ app/models/Customer.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model{
     protected $table="customer";
     protected $primaryKey = 'customer_id';
+    public function addresses(){
+        return $this->hasMany(Address::class,"customer_id");
+    }
 
 }
 

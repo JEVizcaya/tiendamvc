ed23b1999204d8be270f7db451d551dc4e54c82c
JEVizcaya <jevizcaya@hotmail.com>
2025-02-25 17:06:36 +0100

    añadir customer y street  a la base de datos

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -5,13 +5,22 @@ use Formacom\Models\Customer;
 use Formacom\Models\Address;
 class AdminController extends Controller{
     public function index(...$params){
-        
-        $customer=Customer::find(1);
-        var_dump($customer->addresses()->first()->street);
-        exit();
+//crea o encuentra el cliente
+       $customer=new Customer();
+       //asigna atributoa
+       $customer->name= "Joreg E.";
+       $customer->save();
+       //crea una nueva direccion
+       $address=new Address();
+       $address->street="Sobrado de Trives";
+       //asicia direccion al cliente
+       $customer->addresses()->save($address);
+       //opcional asegurarse de que ambos estan guardados
+       $customer->save();
+       $address->save();
        
-        $data = ['mensaje' => '¡Bienvenido a la página de inicio!'];
-        $this->view('home', $data);
+       $data = ['mensaje' => '¡Bienvenido a la página de inicio!'];
+       $this->view('home', $data);
     }
     
 }

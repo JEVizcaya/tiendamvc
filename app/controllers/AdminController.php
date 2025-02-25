c5a3de7ad4908227a7c3721f4e57bb3756bf4cd3
JEVizcaya <jevizcaya@hotmail.com>
2025-02-25 16:09:14 +0100

    probando model customer name

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -1,8 +1,13 @@
 <?php
 namespace Formacom\controllers;
 use Formacom\Core\Controller;
+use Formacom\Models\Customer;
 class AdminController extends Controller{
     public function index(...$params){
+        $customer=new Customer();
+       
+        $customer->name="Antonia";
+        $customer->save();
         $data = ['mensaje' => '¡Bienvenido a la página de inicio!'];
         $this->view('home', $data);
     }
---
+++ app/models/Customer.php
@@ -0,0 +1,10 @@
+<?php
+namespace Formacom\Models;
+use Illuminate\Database\Eloquent\Model;
+class Customer extends Model{
+    protected $table="customer";
+    protected $primaryKey = 'customer_id';
+
+}
+
+?>

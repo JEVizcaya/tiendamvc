8da73f48708c28268053ca38791649392544ab77
JEVizcaya <jevizcaya@hotmail.com>
2025-02-25 16:28:53 +0100

    probando modelo address

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -1,13 +1,16 @@
 <?php
 namespace Formacom\controllers;
 use Formacom\Core\Controller;
-use Formacom\Models\Phone;
+use Formacom\Models\Address;
 class AdminController extends Controller{
     public function index(...$params){
-        $phone=new Phone();
+        $address=new Address();
        
-        $phone->number="999999999";
-        $phone->save();
+        $address->street="Torrecedeira nº 11";
+        $address->zip_code="36206";
+        $address->city="Vigo";
+        $address->country="España";
+        $address->save();
         $data = ['mensaje' => '¡Bienvenido a la página de inicio!'];
         $this->view('home', $data);
     }
